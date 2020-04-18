4ecba8307785378ca07059761c437262fe1e95fb
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-17 20:38:59 -0500

    [Feature] User routes configured and tested

--- node-express-mongo/app/server.js
+++ node-express-mongo/app/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const { PORT } = require('./config/keys');
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('Home page');
 });
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => console.log(`Server connected to port 3000`));
